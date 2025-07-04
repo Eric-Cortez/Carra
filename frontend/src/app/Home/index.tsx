29e3e163d0d505ae45b40a7a718971c75566ffea
Eric-Cortez <ecrcortez@gmail.com>
2025-07-03 22:26:35 -0700

    feat: Add search bar to home page for questions

--- frontend/src/app/Home/index.tsx
+++ frontend/src/app/Home/index.tsx
@@ -1,91 +1,58 @@
-import { Badge } from "@/components/ui/badge";
 import type React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import type { Question } from "@/features/questions/questionSlice";
 import { loadQuestionsAsync } from "@/features/questions/questionSlice";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import type { RootState } from "@/app/store";
 import AskQuestionModalBtn from "@/components/AskQuestionModal";
 import { useUnauthorizedRedirect } from "../authHooks";
-import type { Topic } from "@/features/topics/topicSlice";
-import { loadTopicsAsync } from "@/features/topics/topicSlice";
-import { Link } from "react-router-dom";
 import QuestionCard from "@/components/QuestionsCard";
+import SearchBar from "@/components/SearchBar";
 
 const Home: React.FC = () => {
   const { questions, status, error } = useAppSelector(
     (state: RootState) => state.questions,
   );
-  const { topics } = useAppSelector((state: RootState) => state.topics);
   useUnauthorizedRedirect(error);
   const dispatch = useAppDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     dispatch(loadQuestionsAsync());
-    dispatch(loadTopicsAsync());
   }, [dispatch]);
 
-  const getTopicsWithQuestionCount = (
-    topics: Topic[],
-    questions: Question[],
-  ) => {
-    if (!topics || !questions) return [];
-
-    const topicCounts = topics.map(topic => ({
-      ...topic,
-      questionCount: questions.filter(q => q.topicId === topic.id).length,
-    }));
-
-    return topicCounts.sort((a, b) => b.questionCount - a.questionCount);
-  };
-
-  const topicWithCount = getTopicsWithQuestionCount(topics, questions);
+  const filteredQuestions = questions.filter(question =>
+    question.title.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
 
   return (
-    <div className="flex">
-      <div className="flex flex-col h-fit items-center justify-center m-4 border rounded-lg">
-        <span className="font-semibold bottom-1 p-4">Topics</span>
-        <div className="inline-block">
-          {topicWithCount &&
-            topicWithCount.map(topic => (
-              <Link
-                to={`/topics/${topic.id}`} // Link to the topic page
-                key={`${topic.id}`}
-                className="flex items-center py-2 px-4 hover:bg-secondary/80"
-              >
-                <span>{topic.name}</span>
-                {topic.questionCount > 0 && (
-                  <Badge className="bg-gray-200 text-gray-800 mx-2">
-                    {topic.questionCount}
-                  </Badge>
-                )}
-              </Link>
-            ))}
+    <>
+      <div className="flex items-center justify-end p-6">
+        <div className="w-1/2 mr-4">
+          <SearchBar onSearch={setSearchQuery} />
+        </div>
+        <AskQuestionModalBtn />
+      </div>
+      <div className="flex items-center justify-center p-6">
+        {status === "loading" && <p>Loading...</p>}
+        {status === "failed" && <p>Error: {error}</p>}
+        <div className="flex flex-col  items-center justify-start space-y-6">
+          {filteredQuestions.length > 0
+            ? filteredQuestions.map(quest => (
+                <QuestionCard
+                  key={quest.id}
+                  id={quest.id}
+                  title={quest.title}
+                  content={quest.content}
+                  topicId={quest.topicId}
+                  createdAt={quest.createdAt}
+                  
+                />
+              ))
+            : "No questions found."}
         </div>
       </div>
-      <div>
-        <div className="flex items-center justify-end p-6">
-          <AskQuestionModalBtn />
-        </div>
-        <div className="flex items-center justify-center p-6">
-          {status === "loading" && <p>Loading...</p>}
-          {status === "failed" && <p>Error: {error}</p>}
-          <div className="flex flex-col  items-center justify-start space-y-6">
-            {questions
-              ? questions.map(quest => (
-                  <QuestionCard
-                    key={quest.id}
-                    title={quest.title}
-                    content={quest.content}
-                    topicId={quest.topicId}
-                    createdAt={quest.createdAt}
-                  />
-                ))
-              : "Be the first to ask a question!"}
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
